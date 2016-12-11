f486f13352cd6bc92ab8cb263e719f26caed6709
nms377 <nms377@nyu.edu>
2016-12-10 18:50:00 -1000

    js/shopping_test.js

--- js/shopping_test.js
+++ js/shopping_test.js
@@ -1,1 +1,13 @@
-console.log('shopping list item test')
+console.log('**mocha**', mocha);
+console.log('**chai**', chai);
+console.log('**describe**', describe);
+
+const should = chai.should();
+const expect = chai.should();
+
+describe('ShoppingListItem', () => {
+	let listItem = new ShoppingListItem();
+	it('should be a class', () => {
+		ShoppingListItem.should.be.a.function;
+	})
+});
