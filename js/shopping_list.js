f44d3300f1e2de51a0c86d1f923307d9491aa06c
Andrea <takamiya.andrea@gmail.com>
2016-12-12 23:23:36 -1000

    Was missing a reference to the shoppinglist.js script, added it in

---
+++ js/shopping_list.js
@@ -0,0 +1,21 @@
+ /*jshint esversion: 6 */
+
+class ShoppingList {
+  constructor(items) {
+    this.items = [];
+  }
+
+  addItem(ShoppingListItem){
+    this.ShoppingListItem = ShoppingListItem;
+    this.items.push(ShoppingListItem);
+    console.log(this.items, 'additem');
+  }
+
+  removeItem(ShoppingListItem){
+
+    if(this.items.indexOf(ShoppingListItem) > -1) {
+      this.items.splice(this.items.indexOf(ShoppingListItem), 1);
+    }
+    console.log(this.items, 'removeItem');
+  }
+}
--- js/shopping_list_item.js
+++ js/shopping_list_item.js
@@ -1,6 +1,22 @@
 class ShoppingListItem {
-		constructor () {
+	constructor (name, description, is_done) {
+    this.name = name;
+    this.description = description;
+    this.is_done = is_done;
+	}
 
-		}
+  check() {
+    this.is_done = true;
+    return this.is_done;
+  }
+
+  uncheck() {
+    this.is_done = false;
+    return this.is_done;
+  }
+
+  render() {
+    var renderToString = `<ul>\n<li class='completed_${this.is_done}'>\n<span>${this.name}</span>\n<span>${this.description}</span>\n</li>\n</ul>`
+    return renderToString;
+  }
 }
-
