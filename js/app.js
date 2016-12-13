60ba670a83859528b50d88f8f231d1ef6a554d2a
Andrea <takamiya.andrea@gmail.com>
2016-12-12 22:29:46 -1000

    Created an instance of ShoppingListItem

--- js/app.js
+++ js/app.js
@@ -1,12 +1,10 @@
-// // (function(window){
+var listItem = new ShoppingListItem('Avocado', 'is Squishy', true);
 
-// const ShoppingList = require('/ShoppingList');
+//invoke shopping_list object's render() method
+//store the output to a variable
+//write the output html into the div id=content
 
+var renderListItem = listItem.render()
+document.getElementById('content').innerHTML = renderListItem;
 
-// var shoppingList = new ShoppingListItem()
-
-
-
-// console.log('test')
-
-// // }(window));
+console.log('test', listItem);
