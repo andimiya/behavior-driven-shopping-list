3f0baec0368e0ea3ef5cf197ed89a2cf0fbe3095
Andrea <takamiya.andrea@gmail.com>
2016-12-12 21:28:21 -1000

    Realized that the ShoppingList class should've gone in the shopping_list.js file instead, moved it over

--- js/shopping_list_item.js
+++ js/shopping_list_item.js
@@ -20,23 +20,3 @@ class ShoppingListItem {
     return renderToString;
   }
 }
-
-class ShoppingList {
-  constructor(items) {
-    this.items = [];
-  }
-
-  addItem(ShoppingListItem){
-    this.ShoppingListItem = ShoppingListItem;
-    this.items.push(ShoppingListItem);
-    console.log(this.items, 'additem');
-  }
-
-  removeItem(ShoppingListItem){
-
-    if(this.items.indexOf(ShoppingListItem) > -1) {
-      this.items.splice(this.items.indexOf(ShoppingListItem), 1);
-    }
-    console.log(this.items, 'removeItem');
-  }
-}
