b46fc45293f9a6431defbd439248fc032098dd0a
Andrea <takamiya.andrea@gmail.com>
2016-12-12 22:31:26 -1000

    Rendered the listItem to show the first item in the browser

--- js/app.js
+++ js/app.js
@@ -1,9 +1,5 @@
 var listItem = new ShoppingListItem('Avocado', 'is Squishy', true);
 
-//invoke shopping_list object's render() method
-//store the output to a variable
-//write the output html into the div id=content
-
 var renderListItem = listItem.render()
 document.getElementById('content').innerHTML = renderListItem;
 
