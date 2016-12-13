2eab68e0f7bd1e110395a212c78383b44f48bb1d
Andrea <takamiya.andrea@gmail.com>
2016-12-12 22:46:52 -1000

    Added pseudo-code for the add_to_shopping_list function instructions, going to try merge commits

--- js/app.js
+++ js/app.js
@@ -4,3 +4,11 @@ var renderListItem = listItem.render()
 document.getElementById('content').innerHTML = renderListItem;
 
 console.log('test', listItem);
+
+function add_to_shopping_list(title, desciption) //read the value of the title and description fields from ShoppingListItem (call??)
+//store the result of constructing new ShoppingListItems in the new var new_shopping_list_item
+
+ {
+  var new_shopping_list_item =
+
+}
