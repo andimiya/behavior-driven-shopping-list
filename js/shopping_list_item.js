74405f71c7edf24e4f135246f1fdd50559edc485
nms377 <nms377@nyu.edu>
2016-12-14 00:16:30 -1000

    Don't recall what I did in here...

--- js/shopping_list_item.js
+++ js/shopping_list_item.js
@@ -23,7 +23,7 @@ class ShoppingListItem {
 
 //idx = position (array index) of the shoppingListItem
 //checkbox = actual checkbox element
-    var renderToString = `<br /><span class='completed_${this.is_done}'>\<n<span><input type="checkbox" id="check-box" onclick="changeCheckedStatus()"/></span>\n<span>${this.name}</span>\n<span>${this.description}</span>\n</span>`;
+    var renderToString = `<br /><span class='completed_${this.is_done}'>\<n<span><input type="checkbox" id="check-box" onclick="changeCheckedStatus()"/></span>\n<span>${this.name}</span>\n<span>${this.description}</span>\n<span><button id="removeItem" onclick="removeItemButtonClicked()">X</button></span>\n</span>`;
     return renderToString;
   }
 }
