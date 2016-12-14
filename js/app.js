a231c3b1daa6283c63a3badad23bdc00f6b144ee
nms377 <nms377@nyu.edu>
2016-12-13 16:33:48 -1000

    Create var new_shopping_list_item and invoked ShoppingList addItem method to add a new item with title and description.

--- js/app.js
+++ js/app.js
@@ -1,15 +1,16 @@
 var listItem = new ShoppingListItem('Avocado', 'is Squishy', true);
 
-var renderListItem = listItem.render()
+var renderListItem = listItem.render();
 document.getElementById('content').innerHTML = renderListItem;
 
 console.log('test', listItem);
 
+var addList = new ShoppingList();
+
 class add_to_shopping_list extends ShoppingListItem {
-  constructor (title, desciption) {
-    super(title, desciption);
-  }
+  constructor (title, description) {
+    super(title, description);
+    var new_shopping_list_item = [this.title, this.description];
+    addList.addItem(new_shopping_list_item);
+     }
 }
-
-  // var new_shopping_list_item =
-
